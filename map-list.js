3b589255f69e8b838dceb64901e9c35302e103e0
MorganHuegel <mhue2910@gmail.com>
2018-09-10 20:27:07 -0400

    created dummy data for map-questions

---
+++ map-list.js
@@ -0,0 +1,90 @@
+'use strict';
+const northAmerica = [
+  {
+    id: '000000000000000000000001',
+    url: 'http://www.freeworldmaps.net/northamerica/canada/location.gif', 
+    country: 'canada'
+  },
+  {
+    id: '000000000000000000000002',
+    url: 'http://www.freeworldmaps.net/northamerica/united-states/location.gif', 
+    country: 'united states of america'
+  },
+  {
+    id: '000000000000000000000003',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Mexico_in_the_world_%28W3%29.svg', 
+    country: 'mexico'
+  },
+  {
+    id: '000000000000000000000004',
+    url: 'http://www.freeworldmaps.net/centralamerica/cuba/location.gif', 
+    country: 'cuba'
+  },
+  {
+    id: '000000000000000000000005',
+    url: 'https://i3.wp.com/maps.maphill.com/bahamas/location-maps/savanna-style-map/highlighted-continent/savanna-style-location-map-of-bahamas-highlighted-continent.jpg?fit=450,300&zoom=2&strip=all', 
+    country: 'bahamas'
+  },
+  {
+    id: '000000000000000000000006',
+    url: 'http://maps.maphill.com/el-salvador/location-maps/savanna-style-map/savanna-style-location-map-of-el-salvador.jpg', 
+    country: 'el salvador'
+  },
+  {
+    id: '000000000000000000000007',
+    url: 'http://maps.maphill.com/haiti/location-maps/gray-map/highlighted-continent/gray-location-map-of-haiti-highlighted-continent.jpg', 
+    country: 'Haiti'
+  },
+  {
+    id: '000000000000000000000008',
+    url: 'https://www.liveandinvestoverseas.com/wp-content/uploads/2015/08/map-of-panama.jpg', 
+    country: 'panama'
+  },
+  {
+    id: '000000000000000000000009',
+    url: 'http://maps.maphill.com/jamaica/location-maps/savanna-style-map/highlighted-continent/savanna-style-location-map-of-jamaica-highlighted-continent.jpg', 
+    country: 'jamaica'
+  },
+  {
+    id: '000000000000000000000010',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Greenland_in_the_world_%28W3%29.svg/2000px-Greenland_in_the_world_%28W3%29.svg.png',
+    country: 'greenland'
+  }
+]
+
+const mapUrls = [
+  //North America
+  /* Antigua and Barbuda, Bahamas, Barbados, Belize, Canada, Costa Rica, Cuba, Dominica
+  Dominican Republic, El Salvador, Grenada, Guatemala, Haiti, Honduras, Jamaica
+  Mexico, Nicaragua, Panama, Saint Kitts and Nevis, Saint Lucia, Saint Vincent and the Grenadines
+  Trinidad and Tobago, United States of America */
+  {url: 'http://www.freeworldmaps.net/northamerica/canada/location.gif', country: 'canada'},
+  {url: 'http://www.freeworldmaps.net/northamerica/united-states/location.gif', country: 'united states of america'},
+  {url: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Mexico_in_the_world_%28W3%29.svg', country: 'mexico'},
+  {url: 'http://www.freeworldmaps.net/centralamerica/cuba/location.gif', country: 'cuba'},
+  {url: 'http://static.maphill.com/12/img/t.gif', country: 'bahamas'},
+  {url: 'http://maps.maphill.com/el-salvador/location-maps/savanna-style-map/savanna-style-location-map-of-el-salvador.jpg', country: 'el salvador'},
+  {url: 'http://maps.maphill.com/haiti/location-maps/gray-map/highlighted-continent/gray-location-map-of-haiti-highlighted-continent.jpg', country: 'Haiti'},
+  {url: 'https://www.liveandinvestoverseas.com/wp-content/uploads/2015/08/map-of-panama.jpg', country: 'panama'},
+  {url: 'http://maps.maphill.com/jamaica/location-maps/savanna-style-map/highlighted-continent/savanna-style-location-map-of-jamaica-highlighted-continent.jpg', country: 'jamaica'}
+
+  //South America
+
+  //Africa
+
+  //Europe
+
+  //Asia
+
+  //Oceania
+  /* American Samoa, Australia, Cook Islands, Fiji, French Polynesia, Guam,
+  Howland Island, Marshall Islands, Federated States of Micronesia, Nauru,
+  New Caledonia, New Zealand, Niue, Norfolk Island, Northern Mariana Islands,
+  Palau, Papua New Guinea, Pitcairn Islands, Samoa, Solomon Islands, Tokelau,
+  Tonga, Tuvalu, Vanuatu, Wallis and Futuna */
+  'http://www.freeworldmaps.net/australia/location.gif',
+
+
+  //Antarctica
+  'https://upload.wikimedia.org/wikipedia/commons/b/b3/Antarctica_in_the_world_%28red%29_%28W3%29.svg'
+];
